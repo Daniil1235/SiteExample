55c9ddcbc0d863f9ae3b6b29e2bdd066a51f00f5
daniil <bialetskidaniil@gmail.com>
2025-01-16 20:36:55 +0300

    login&logout&done

--- main/views.py
+++ main/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Task, Priority
 from .forms import TaskForm, NewUserForm
 from django.views.generic import UpdateView, DeleteView
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 
@@ -29,18 +30,17 @@ def index(request):
 
 
 def create(request):
-    error = ''
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect("home")
         else:
-            error = 'При отправке формы произошла ошибка.'
+            messages.error(request, "При отправке формы произошла ошибка")
     form = TaskForm()
     action = "Добавление"
     action_v = "Добавить"
-    return render(request, "main/create.html", {"form": form, "error": error, "action": action, "action_v": action_v})
+    return render(request, "main/create.html", {"form": form, "action": action, "action_v": action_v})
 
 
 def detail_view(request, id):
@@ -59,3 +59,28 @@ def register_request(request):
         messages.error(request, "При регистрации произошла ошибка. Убедитесь, что данные введены верно")
     form = NewUserForm()
     return render(request=request, template_name="main/register.html", context={"register_form": form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Вы успешно вошли как {username}")
+                return redirect("home")
+            else:
+                messages.error(request, "Неверное имя или пароль")
+        else:
+            messages.error(request, "Неверное имя или пароль")
+    form = AuthenticationForm()
+    return render(request=request, template_name="main/login.html", context={"login_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.warning(request, "Вы вышли из аккаунта")
+    return redirect("home")
